8b936c9a2a9b336a96cdbbd58417f0b1c22e94b3
Nguyễn Anh Tuấn <nguyenanhtuan6955@gmail.com>
2019-04-06 15:23:45 +0700

    create website calculator

---
+++ public/javascripts/javascript.js
@@ -0,0 +1,60 @@
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+function mul(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
+function calculate() {
+    document.getElementById("thongbao").innerHTML="";
+    document.getElementById("kq").value ="";
+    if(document.getElementById("so1").value=="")
+    {
+        document.getElementById("thongbao").innerHTML="Mời nhập số thứ nhất";
+        return;
+    } 
+    if(document.getElementById("so2").value=="")
+    {
+        document.getElementById("thongbao").innerHTML="Mời nhập số thứ hai";
+        return;
+    } 
+    var a = parseFloat(document.getElementById("so1").value);
+    var b = parseFloat(document.getElementById("so2").value);
+    if(isNaN(document.getElementById("so1").value-a)||isNaN(document.getElementById("so2").value-b))
+    {
+        document.getElementById("thongbao").innerHTML="Kí tự nhập vào không phải số";
+        return;
+    }
+    var cacpheptinh=document.getElementsByName("toantu");
+    var len=cacpheptinh.length;
+    var pt="no choice";
+
+    for (let i = 0; i < len; i++) {
+        if(cacpheptinh[i].checked)
+            pt=cacpheptinh[i].value;
+        
+    }
+    // alert(pt);
+    switch (pt) {
+        case "Cộng":
+            document.getElementById("kq").value = add(a, b);
+            break;
+        case "Trừ":
+            document.getElementById("kq").value = sub(a, b);
+            break;
+        case "Nhân":
+            document.getElementById("kq").value = mul(a, b);
+            break;
+        case "Chia":
+            document.getElementById("kq").value = div(a, b);
+            break;
+        default:
+            document.getElementById("thongbao").innerHTML="Mời nhập phép toán";
+            break;
+    }
+}
